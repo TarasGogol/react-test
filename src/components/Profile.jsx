f212db79fef365f907a9e07c63565f29154781c2
TarasGogol <vip.kristals@gmail.com>
2022-10-02 18:19:31 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg" />
+      </div>
+      <div>ava + decription</div>
+      <div>
+        my post
+        <div>New post</div>
+      </div>
+      <div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
